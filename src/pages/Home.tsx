e3925e71d527137a04c4fdc5b04bed0ffe0225c9
Lívia Minucelli <liviawrk@gmail.com>
2021-07-01 15:10:07 -0300

    Updating project letmeask

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import { useHistory } from 'react-router-dom'
 
-import { auth, firebase } from '../services/firebase'
+import { useAuth } from '../hooks/useAuth';
 
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg';
@@ -8,20 +8,23 @@ import googleIconImg from '../assets/images/google-icon.svg';
 
 import { Button } from '../components/Button';
 
+
 import '../styles/auth.scss';
+
+
+
 
 
 export function Home() {
     const history = useHistory();
+    const { user, signInWithGoogle } = useAuth();
 
-    function handleCreateRoom() {
-        const provider = new firebase.auth.GoogleAuthProvider();
+    async function handleCreateRoom() {
+        if (!user) {
+            await signInWithGoogle()
+        }
 
-        auth.signInWithPopup(provider).then(result => {
-            console.log(result)
-        })
-
-        history.push('/rooms/new'); //loginFFFF 55:00
+        history.push('/rooms/new');
     }
 
     return (
