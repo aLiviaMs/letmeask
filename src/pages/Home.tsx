21fe8dcd0197d92941644b79e9c4a4975f5d9591
Lívia Minucelli <liviawrk@gmail.com>
2021-07-10 12:43:21 -0300

    .

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,21 @@
 import { useHistory } from 'react-router-dom'
-import { FormEvent } from 'react';
-import { useAuth } from '../hooks/useAuth';
+import { FormEvent, useState } from 'react';
 
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
 
-import { Button } from '../components/Button';
-
-
-import '../styles/auth.scss';
-import { useState } from 'react';
 import { database } from '../services/firebase';
 
+import { Button } from '../components/Button';
+import { useAuth } from '../hooks/useAuth';
+
+import '../styles/auth.scss';
 
 export function Home() {
     const history = useHistory();
-    const { user, signInWithGoogle } = useAuth();
-    const [roomCode, setRoomCode] = useState('')
+    const { user, signInWithGoogle } = useAuth()
+    const [roomCode, setRoomCode] = useState('');
 
     async function handleCreateRoom() {
         if (!user) {
@@ -41,6 +39,11 @@ export function Home() {
             return;
         }
 
+        if (roomRef.val().endedAt) {
+            alert('Room already closed.');
+            return;
+        }
+
         history.push(`/rooms/${roomCode}`);
     }
 
@@ -55,7 +58,7 @@ export function Home() {
                 <div className="main-content">
                     <img src={logoImg} alt="Letmeask" />
                     <button onClick={handleCreateRoom} className="create-room">
-                        <img src={googleIconImg} alt="Logo Google" />
+                        <img src={googleIconImg} alt="Logo do Google" />
                         Crie sua sala com o Google
                     </button>
                     <div className="separator">ou entre em uma sala</div>
